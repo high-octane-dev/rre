ffd85040665777d1a80996032ecb28d6b66bb4d3
TKFRvisionOfficial <52245132+TKFRvisionOfficial@users.noreply.github.com>
2025-01-15 23:46:45 +0100

    Maybe fixed Svformatter signature

--- source/util/rsstring_util.cpp
+++ source/util/rsstring_util.cpp
@@ -1,13 +1,13 @@
 #include "rsstring_util.hpp"
 
-// I MIGHT BE TOTALLY WRONG ABOUT THE SIGNATURE
 // OFFSET: 0x0055f230
 int RSStringUtil::Ssnprintf(char* buf, int len, const char* format, ...) {
 	// TODO: Not yet implemented
 	return len;
 }
 
+// I am so unsure about this signature...
 // OFFSET: 0x0055e600
-void RSStringUtil::Svformatter(int (*format_function)(), char* buf, const char* format, const void* format_args) {
+void RSStringUtil::Svformatter(int (*format_function)(char**), char* buf, const char* format, const void* format_args) {
 	// TODO: Not yet implemented
 }
--- source/util/rsstring_util.hpp
+++ source/util/rsstring_util.hpp
@@ -2,5 +2,5 @@
 
 namespace RSStringUtil {
 	int Ssnprintf(char* buf, int len, const char* format, ...);
-	void Svformatter(int (*format_function)(), char* buf, const char* format, const void* format_args);
+	void Svformatter(int (*format_function)(char**), char* buf, const char* format, const void* format_args);
 }
