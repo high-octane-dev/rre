a0c6f7fc25507ed8161f0885316c518f41142212
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2025-01-14 21:39:17 -0500

    [D3DStateManager] finish state cache member funcs

--- source/gfx/d3d_state_manager.cpp
+++ source/gfx/d3d_state_manager.cpp
@@ -7,36 +7,114 @@ D3DStateManager* lpD3DStateManager = nullptr;
 D3DStateManager::D3DStateManager() {
 	D3DCAPS9 caps{};
 	g_D3DDevice9->GetDeviceCaps(&caps);
-	max_registers = 0x10;
+	max_samplers = 16;
 	fullscreen_effects_enabled = 0;
-	sampler_states = new SamplerState[16];
-	unk_sampler_indexed1 = new unsigned int [max_registers];
-	unk_sampler_indexed2 = new unsigned int [max_registers];
+	sampler_states = new SamplerStateCache[max_samplers];
+	min_sampler_state_type_used = new unsigned int [max_samplers];
+	max_sampler_state_type_used = new unsigned int [max_samplers];
 	Reset();
 }
 
 // OFFSET: 0x00415550
 D3DStateManager::~D3DStateManager() {
 	delete[] sampler_states;
-	delete[] unk_sampler_indexed1;
-	delete[] unk_sampler_indexed2;
+	delete[] min_sampler_state_type_used;
+	delete[] max_sampler_state_type_used;
 }
 
 // OFFSET: 0x00415590
 void D3DStateManager::Reset() {
+	for (auto& render_state : render_states) {
+		render_state.value = -1;
+		render_state.previous_value = -1;
+	}
+	for (std::size_t i = 0; i < 16; i++) {
+		for (auto& sampler_state_variable : sampler_states[i].sampler_desc) {
+			sampler_state_variable.value = -1;
+			sampler_state_variable.previous_value = -1;
+		}
+	}
+	g_D3DDevice9->SetRenderState(D3DRS_ALPHAREF, 0);
+	render_states[D3DRS_ALPHAREF].value = 0;
+	render_states[D3DRS_ALPHAREF].previous_value = 0;
+	min_render_state = MAX_RENDER_STATE_TYPE;
+	max_render_state = 0;
+	min_sampler_used = max_samplers;
+	max_sampler_state_type_used = 0;
+	for (std::size_t i = 0; i < max_samplers; i++) {
+		min_sampler_state_type_used[i] = MAX_SAMPLER_STATE_TYPE;
+		max_sampler_state_type_used[i] = 0;
+	}
 }
 
 // OFFSET: 0x00415660
 void D3DStateManager::SendData() {
+	if (min_render_state != MAX_RENDER_STATE_TYPE) {
+		CachedValue* render_state = &render_states[min_render_state];
+		for (std::size_t i = 0; i <= max_render_state - min_render_state; i++) {
+			std::size_t render_state_index = min_render_state + i;
+			DWORD value_to_set = render_state->value;
+
+			if (fullscreen_effects_enabled && (render_state_index == D3DRS_ALPHATESTENABLE || render_state_index == D3DRS_ALPHABLENDENABLE)) {
+				value_to_set = 0;
+			}
+
+			if (value_to_set != render_state->previous_value) {
+				g_D3DDevice9->SetRenderState(static_cast<D3DRENDERSTATETYPE>(render_state_index), value_to_set);
+				render_state->previous_value = value_to_set;
+			}
+
+			render_state++;
+		}
+		min_render_state = MAX_RENDER_STATE_TYPE;
+		max_render_state = 0;
+	}
+
+	if (min_sampler_used != max_samplers) {
+		for (std::size_t sampler_index = min_sampler_used; sampler_index <= max_sampler_used; sampler_index++) {
+			CachedValue* sampler_state = &sampler_states[sampler_index].sampler_desc[min_sampler_state_type_used[sampler_index]];
+			for (std::size_t i = 0; i <= max_sampler_state_type_used[sampler_index] - min_sampler_state_type_used[sampler_index]; ++i) {
+				std::size_t sampler_state_type = min_sampler_state_type_used[sampler_index] + i;
+
+				if (sampler_state->value != sampler_state->previous_value) {
+					g_D3DDevice9->SetSamplerState(sampler_index, static_cast<D3DSAMPLERSTATETYPE>(sampler_state_type), sampler_state->value);
+					sampler_state->previous_value = sampler_state->value;
+				}
+
+				sampler_state++;
+			}
+
+			min_sampler_state_type_used[sampler_index] = MAX_SAMPLER_STATE_TYPE;
+			max_sampler_state_type_used[sampler_index] = 0;
+		}
+
+		min_sampler_used = max_samplers;
+		max_sampler_used = 0;
+	}
 }
 
 // OFFSET: 0x004025c0
 void D3DStateManager::SetRenderState(D3DRENDERSTATETYPE type, DWORD value) {
 	g_D3DDevice9->SetRenderState(type, value);
-	render_states[type].state_value = value;
-	render_states[type].set_state_value = value;
+	render_states[type].value = value;
+	render_states[type].previous_value = value;
 }
 
 // OFFSET: 0x00404bd0
-void D3DStateManager::SetSamplerState(D3DSAMPLERSTATETYPE sampler_state, D3DRENDERSTATETYPE render_state, DWORD value) {
+void D3DStateManager::SetSamplerState(DWORD sampler_index, D3DSAMPLERSTATETYPE sampler_state_type, DWORD value) {
+	sampler_states[sampler_index].sampler_desc[sampler_state_type].value = value;
+	if (value != sampler_states[sampler_index].sampler_desc[sampler_state_type].previous_value) {
+		if (sampler_index < min_sampler_used) {
+			min_sampler_used = sampler_index;
+		}
+		if (max_sampler_used < sampler_index) {
+			max_sampler_used = sampler_index;
+		}
+		if (sampler_state_type < min_sampler_state_type_used[sampler_index]) {
+			min_sampler_state_type_used[sampler_index] = sampler_state_type;
+		}
+		if (max_sampler_state_type_used[sampler_index] < sampler_state_type) {
+			max_sampler_state_type_used[sampler_index] = sampler_state_type;
+		}
+	}
 }
--- source/gfx/d3d_state_manager.hpp
+++ source/gfx/d3d_state_manager.hpp
@@ -2,36 +2,30 @@
 #include <array>
 #include <d3d9.h>
 
+constexpr std::size_t MAX_RENDER_STATE_TYPE = static_cast<std::size_t>(D3DRS_BLENDOPALPHA) + 1;
+constexpr std::size_t MAX_SAMPLER_STATE_TYPE = 48;
+
 class D3DStateManager {
 public:
-	struct RenderStateCache {
-		int state_value;
-		int set_state_value;
-	};
-
 	struct CachedValue {
-		int value;
-		int set_value;
+		DWORD value;
+		DWORD previous_value;
 	};
 
 	struct SamplerStateCache {
-		CachedValue Description[12];
-	};
-
-	struct SamplerState {
-		std::array<SamplerStateCache, 16> render_states;
+		std::array<CachedValue, 48> sampler_desc;
 	};
 
 	unsigned int fullscreen_effects_enabled;
-	std::array<RenderStateCache, 210> render_states;
-	SamplerState* sampler_states;
-	unsigned int* unk_sampler_indexed1;
-	unsigned int* unk_sampler_indexed2;
-	unsigned int max_registers;
-	unsigned int state_index;
-	unsigned int unk_state_index;
-	unsigned int sampler_state_index;
-	unsigned int max_register_index;
+	std::array<CachedValue, MAX_RENDER_STATE_TYPE> render_states;
+	SamplerStateCache* sampler_states;
+	unsigned int* min_sampler_state_type_used;
+	unsigned int* max_sampler_state_type_used;
+	unsigned int max_samplers;
+	unsigned int min_render_state;
+	unsigned int max_render_state;
+	unsigned int min_sampler_used;
+	unsigned int max_sampler_used;
 public:
 	D3DStateManager();
 	~D3DStateManager();
@@ -40,7 +34,7 @@ public:
 	void Reset();
 	void SendData();
 	void SetRenderState(D3DRENDERSTATETYPE type, DWORD value);
-	void SetSamplerState(D3DSAMPLERSTATETYPE sampler_state, D3DRENDERSTATETYPE render_state, DWORD value);
+	void SetSamplerState(DWORD sampler_index, D3DSAMPLERSTATETYPE sampler_state_type, DWORD value);
 
 };
 
