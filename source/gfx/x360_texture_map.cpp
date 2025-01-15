792a6f4b5d6e4317f6eef0c5807a16d1b493372d
Jordon87 <38020644+Jordon87@users.noreply.github.com>
2025-01-15 20:41:47 +1300

    wip texture maps

---
+++ source/gfx/x360_texture_map.cpp
@@ -0,0 +1,153 @@
+#include "x360_texture_map.hpp"
+#include <data_access.hpp>
+#include <dxsdk-d3dx\d3dx9tex.h>
+#include "x360_video_card.hpp"
+
+extern int g_EnableEndianSwapping;
+
+// OFFSET: 0x0040f760
+void BaseTextureMap::ClearMaxProjectedSize()
+{
+	unk4 = 0.0f;
+}
+
+// OFFSET: 0x0040f770
+TextureMap* TextureMap::GetData()
+{
+	return this;
+}
+
+// OFFSET: INLINE
+void X360TextureMap::Unk1()
+{
+	return;
+}
+
+// OFFSET: 0x00410100
+int X360TextureMap::Unk2(unsigned int a1)
+{
+	D3DXIMAGE_INFO local_3c;
+
+	//this->field3_0xd0 = 0;
+	lpDataAccess->FRead(a1, &a1, 4, 1);
+	if (g_EnableEndianSwapping == 0)
+	{
+		a1 = (a1 & 0xff0000 | a1 >> 0x10) >> 8 |
+			(a1 & 0xff00 | a1 << 0x10) << 8;
+	}
+	void* pSrcData = malloc(a1);
+	lpDataAccess->FRead(a1, pSrcData, a1, 1);
+	D3DXGetImageInfoFromFileInMemory(pSrcData, a1, &local_3c);
+	if (local_3c.ResourceType == D3DRTYPE_CUBETEXTURE)
+	{
+		D3DXCreateCubeTextureFromFileInMemoryEx(g_D3DDevice9, pSrcData, a1, local_3c.Width, local_3c.MipLevels, 0, local_3c.Format, D3DPOOL_MANAGED, -1, -1, 0, nullptr, nullptr, &cube_texture);
+	}
+	else
+	{
+		D3DXCreateTextureFromFileInMemoryEx(g_D3DDevice9, pSrcData, a1, local_3c.Width, local_3c.Height, local_3c.MipLevels, 0, local_3c.Format, D3DPOOL_MANAGED, -1, -1, 0, nullptr, nullptr, &texture);
+	}
+	free(pSrcData);
+	/*
+	pIVar2 = (IDirect3DCubeTexture9 *)this->texture;
+	if (pIVar2 == (IDirect3DCubeTexture9 *)0x0) {
+		pIVar2 = this->cube_texture;
+	}
+	(*(code *)((IDirect3DTexture9_VTable *)pIVar2->field0_0x0)->GetLevelDesc)
+			((IDirect3DTexture9 *)pIVar2,0,&local_20);
+	(this->TextureMap).BaseTextureMap.field3_0xe = (word)local_20.Height;
+	(this->TextureMap).field4_0x38 = local_20.Format;
+	(this->TextureMap).BaseTextureMap.field2_0xc = (word)local_20.Width;
+	*/
+
+	return 1;
+}
+
+// OFFSET: 0x00442ef0
+int X360TextureMap::Unk3()
+{
+	return 0;
+}
+
+// OFFSET: 0x00410260
+int X360TextureMap::Unk4(void*, int, int, int, int, int, D3DFORMAT, unsigned int)
+{
+	return 0;
+}
+
+// OFFSET: 0x0040fb90
+int X360TextureMap::Unk5()
+{
+	return 0;
+}
+
+// OFFSET: INLINE
+int X360TextureMap::Unk6()
+{
+	return 1;
+}
+
+// OFFSET: INLINE
+int X360TextureMap::Unk7()
+{
+	return 1;
+}
+
+// OFFSET: INLINE
+int X360TextureMap::Unk8()
+{
+	return 1;
+}
+
+// OFFSET: INLINE
+void X360TextureMap::Unk9()
+{
+	return;
+}
+
+// OFFSET: INLINE
+void X360TextureMap::Unk10()
+{
+	return;
+}
+
+// OFFSET: 0x0040fde0
+int X360TextureMap::Unk11(int, int, int*)
+{
+	return 0;
+}
+
+// OFFSET: 0x0040fe30
+int X360TextureMap::Unk12()
+{
+	return 0;
+}
+
+// OFFSET: 0x0040fcb0
+int X360TextureMap::Unk13(int)
+{
+	return 0;
+}
+
+// OFFSET: 0x00410570
+int* X360TextureMap::Unk14(int)
+{
+	return nullptr;
+}
+
+// OFFSET: 0x004105c0
+int X360TextureMap::Unk15(int)
+{
+	return 0;
+}
+
+// OFFSET: 0x0040fd90
+void X360TextureMap::Unk16(int)
+{
+	return;
+}
+
+// OFFSET: 0x00410e40
+int X360TextureMap::Unk17()
+{
+	return 0;
+}
--- source/gfx/x360_texture_map.hpp
+++ source/gfx/x360_texture_map.hpp
@@ -1,4 +1,70 @@
 #pragma once
+#include "base_object.hpp"
+#include <d3d9.h>
 
-class TextureMap {};
-class X360TextureMap : public TextureMap {};
+class BaseTextureMap : public BaseObject
+{
+	int unk1;
+	short unk2;
+	short unk3;
+	float unk4;
+
+	virtual void ClearMaxProjectedSize();
+};
+
+class TextureMap : public BaseTextureMap
+{
+public:
+	int unk1;
+	int unk2;
+	D3DFORMAT format;
+	int unk3;
+	int unk4;
+	char texture_filename[128];
+	short unk5;
+	short unk6;
+
+	virtual TextureMap* GetData();
+	virtual void Unk1() = 0;
+	virtual int Unk2(unsigned int) = 0;
+	virtual int Unk3() = 0;
+	virtual int Unk4(void *, int, int, int, int, int, D3DFORMAT, unsigned int) = 0;
+	virtual int Unk5() = 0;
+	virtual int Unk6() = 0;
+	virtual int Unk7() = 0;
+	virtual int Unk8() = 0;
+	virtual void Unk9() = 0;
+	virtual void Unk10() = 0;
+	virtual int Unk11(int, int, int*) = 0;
+	virtual int Unk12() = 0;
+	virtual int Unk13(int) = 0;
+	virtual int * Unk14(int) = 0;
+	virtual int Unk15(int) = 0;
+	virtual void Unk16(int) = 0;
+	virtual int Unk17() = 0;
+};
+
+class X360TextureMap : public TextureMap
+{
+public:
+	IDirect3DTexture9* texture;
+	IDirect3DCubeTexture9* cube_texture;
+
+	void Unk1();
+	int Unk2(unsigned int);
+	int Unk3();
+	int Unk4(void*, int, int, int, int, int, D3DFORMAT, unsigned int);
+	int Unk5();
+	int Unk6();
+	int Unk7();
+	int Unk8();
+	void Unk9();
+	void Unk10();
+	int Unk11(int, int, int*);
+	int Unk12();
+	int Unk13(int);
+	int* Unk14(int);
+	int Unk15(int);
+	void Unk16(int);
+	int Unk17();
+};
