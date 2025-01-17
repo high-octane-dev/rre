72cd6005b4061950843b50432473818ebbef0936
TKFRvisionOfficial <52245132+TKFRvisionOfficial@users.noreply.github.com>
2025-01-17 11:18:33 +0100

    Added unhelpful comment

--- source/util/rsstring_util.cpp
+++ source/util/rsstring_util.cpp
@@ -7,6 +7,8 @@ int RSStringUtil::Ssnprintf(char* buf, std::size_t len, const char* format, ...)
 	// ft said something about if it walks like a duck its a duck or some shit.
 	// I really don't want to reverse the Svformatter function.
 
+	// Clang gets annoyed here because of the format variable (this can potential enable exploits)
+	// there is probably no way to make it happy.
 	return snprintf(buf, len, format);
 }
 
