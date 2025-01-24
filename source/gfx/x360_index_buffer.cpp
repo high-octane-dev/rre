121c25d7044ff797ec96eeff7d0d6394a3a8cdea
MythicalBry <bryant128499@gmail.com>
2025-01-24 08:58:45 -0500

    changed X360IndexBuffer function def to take in D3D type

--- source/gfx/x360_index_buffer.cpp
+++ source/gfx/x360_index_buffer.cpp
@@ -1,13 +1,13 @@
 #include "x360_index_buffer.hpp"
 
-X360IndexBuffer::X360IndexBuffer(int param1, int param2) {
+X360IndexBuffer::X360IndexBuffer(D3DFORMAT fmt, int param2) {
 
     this->format = D3DFMT_UNKNOWN;
     this->bytes_per_index = 0;
     this->index_count = 0;
-    this->field1_0x4 = 0; 
+    this->field1_0x4 = 0;
 
-    Create(static_cast<D3DFORMAT>(param1), param2);
+    Create(fmt, param2);
 }
 
 int X360IndexBuffer::Create(D3DFORMAT fmt, int index_count) {
--- source/gfx/x360_index_buffer.hpp
+++ source/gfx/x360_index_buffer.hpp
@@ -6,6 +6,6 @@ private:
     IDirect3DIndexBuffer9* index_buffer;
     int field1_0x4;
 public:
-    X360IndexBuffer(int param1, int param2);
+    X360IndexBuffer(D3DFORMAT fmt, int param2);
     int Create(D3DFORMAT fmt, int index_count) override;
 };
